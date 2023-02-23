0f4d908b1f3aa304dd6a3d5499fb69073882f5f8
chiri03 <lena.kiriac.97@gmail.com>
2023-02-24 00:39:31 +0200

    create modal window for "Our Locations"; styled modal window for "our locations; fixed my bug in main.min.scss

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,4 @@
+// modal window pentru "Franchise "
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
@@ -9,7 +10,19 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    console.log(refs);
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
+// modal window pentru "Our location "
+const modal = document.querySelector(".modal_1");
+const openModal = document.querySelector(".open-button_1");
+const closeModal = document.querySelector(".close-button");
+
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
