33842921f23459fad9ae21f034f4564f6880b748
chiri03 <lena.kiriac.97@gmail.com>
2023-02-25 00:26:39 +0200

    remove commented code

--- js/modal.js
+++ js/modal.js
@@ -1,26 +1,8 @@
-// modal window pentru "Franchise "
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector("[data-modal-open]"),
-//     closeModalBtn: document.querySelector("[data-modal-close]"),
-//     modal: document.querySelector("[data-modal]"),
-//   };
-
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
-
-//   function toggleModal() {
-//     refs.modal.classList.toggle("is-hidden");
-//   }
-// })();
-
-// modal window pentru "Our location "
 const locationModal = document.querySelector(".modal_1");
 const openLocationModal = document.querySelector(".open-button_1");
 const closeLocationModal = document.querySelector(".close-button");
 
 openLocationModal.addEventListener("click", () => {
-  console.log("uyvuy");
   locationModal.showModal();
 });
 
@@ -33,9 +15,6 @@ const openFranchiseModal = document.querySelector(".second-btn");
 const closeFranchiseModal = document.querySelector(".close-modal");
 
 openFranchiseModal.addEventListener("click", () => {
-  console.log("uyvuy");
-  console.log(franchiseModal);
-
   franchiseModal.showModal();
 });
 
